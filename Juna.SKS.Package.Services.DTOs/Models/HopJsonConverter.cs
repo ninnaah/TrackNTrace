4a0305f6cb9d60dbdc5452b227a12511a5684369
if19b042 <if19b042.technikum-wien.at>
2021-09-22 16:40:43 +0200

    Add project files.

---
+++ Juna.SKS.Package.Services.DTOs/Models/HopJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juna.SKS.Package.Services.DTOs.Models
+{
+    public class HopJsonConverter : JsonCreationConverter<Hop>
+    {
+        protected override Hop Create(Type objectType, JObject jObject)
+        {
+            if (jObject == null) throw new ArgumentNullException("jObject");
+
+            if (jObject["numberPlate"] != null)
+            {
+                return new Truck();
+            }
+            else if (jObject["level"] != null)
+            {
+                return new Warehouse();
+            }
+            else
+            {
+                return new Transferwarehouse();
+            }
+        }
+    }
+}
