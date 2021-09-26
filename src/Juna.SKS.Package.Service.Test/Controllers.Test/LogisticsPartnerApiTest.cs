6e844ba94c4286984249749d508171d7ede06c8e
if19b042 <if19b042.technikum-wien.at>
2021-09-26 10:39:37 +0200

    added test projekt

---
+++ src/Juna.SKS.Package.Service.Test/Controllers.Test/LogisticsPartnerApiTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Juna.SKS.Package.Services.Test.Controllers.Test
+{
+    public class LogisticsPartnerApiTest
+    {
+        [Test]
+        public void TransitionParcel_ValidParcel_TrackingIdReturned()
+        {
+
+        }
+    }
+}
